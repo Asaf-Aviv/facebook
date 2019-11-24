b34cafcf891c87346ef0e31740ed79c8ecf72276
Asaf Aviv <asafaviv89@gmail.com>
2019-11-24 23:24:46 +0200

    useWindowWidth

---
+++ src/hooks/index.ts
@@ -0,0 +1,2 @@
+export { default as useOnOutsideClick } from './useOnOutsideClick'
+export { default as useWindowWidth } from './useWindowWidth'
---
+++ src/hooks/useWindowWidth.ts
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react'
+import throttle from 'lodash.throttle'
+
+function useWindowWidth() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+
+  useEffect(() => {
+    const handleResize = throttle(() => setWindowWidth(window.innerWidth), 50)
+
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
+  return windowWidth
+}
+
+export default useWindowWidth
