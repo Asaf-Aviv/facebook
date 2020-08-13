b48a074a3669c4ba868b66d8f88dadd65117c15b
Asaf Aviv <asafaviv89@gmail.com>
2020-08-13 15:36:16 +0300

    react-ga

--- src/components/Facebook/Facebook.tsx
+++ src/components/Facebook/Facebook.tsx
@@ -1,18 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { hot } from 'react-hot-loader/root'
 import { BrowserRouter as Router } from 'react-router-dom'
+import ReactGA from 'react-ga'
 import { ThemeProvider } from 'styled-components'
 import { Layout, WindowWidthProdiver } from 'components'
 import theme from 'styles/theme'
 
-const Facebook: React.FC = () => (
-  <Router>
-    <ThemeProvider theme={theme}>
-      <WindowWidthProdiver>
-        <Layout />
-      </WindowWidthProdiver>
-    </ThemeProvider>
-  </Router>
-)
+const Facebook: React.FC = () => {
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search)
+  }, [])
+
+  return (
+    <Router>
+      <ThemeProvider theme={theme}>
+        <WindowWidthProdiver>
+          <Layout />
+        </WindowWidthProdiver>
+      </ThemeProvider>
+    </Router>
+  )
+}
 
 export default hot(Facebook)
