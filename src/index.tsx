6a6d93cc7682af8e61ada8e474732bf2899c32b3
Asaf Aviv <asafaviv89@gmail.com>
2020-08-13 15:19:36 +0300

    add react-ga

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import 'react-hot-loader'
 import React from 'react'
+import ReactGA from 'react-ga'
 import { render } from 'react-dom'
 import { Facebook } from 'components'
 import padStatrtPolyfill from 'polyfills/padStart'
 import * as serviceWorker from './serviceWorker'
 import 'simplebar/dist/simplebar.min.css'
+
+ReactGA.initialize(process.env.REACT_APP_GA!)
 
 padStatrtPolyfill()
 
